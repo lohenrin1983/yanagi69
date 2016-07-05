690246a1106b682819864050b5e3c957197b4669
土屋太 <tsuchiyafutoshi@tsuchiyafutoshi-no-MacBook-Air.local>
2016-07-05 18:04:41 +0900

    indexファイル編集

--- db/migrate/20160705063601_create_comments.rb
+++ db/migrate/20160705063601_create_comments.rb
@@ -1,8 +1,11 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+        t.text        :text
+        t.integer     :user_id
+        t.integer     :tweet_id
 
-      t.timestamps null: false
+      t.timestamps 
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160704080303) do
+ActiveRecord::Schema.define(version: 20160705063601) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "text",       limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.integer  "tweet_id",   limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string   "name",       limit: 255
