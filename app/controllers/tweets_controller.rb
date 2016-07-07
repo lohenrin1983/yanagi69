8a914579aeb6b7b0bbfe30a8a01aac01a5d70f84
土屋太 <tsuchiyafutoshi@tsuchiyafutoshi-no-MacBook-Air.local>
2016-07-07 14:27:12 +0900

    ユーザー機能の改良

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,25 @@
 class TweetsController < ApplicationController
+  
+  before_action :move_to_index, except: :index
   def index
-    @tweets = Tweet.all
+     @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
     #test = %!<iframe width="560" height="315" src="https://www.youtube.com/embed/UtoReZlTvu4" frameborder="0" allowfullscreen></iframe>!
     #@test = test
   end
   def new
     
   end
+  def create
+      Tweet.create(tweet_params)
+    end
 
+    private
+    def tweet_params
+      params.permit(:title, :video, :text)
+    end
+    def move_to_index
+      redirect_to action: :index unless user_signed_in?
+    end
 
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  #devise_for :users
+  devise_for :users
+  root  'tweets#index'
   get 'tweets' => 'tweets#index'
   get 'tweets/new'  =>  'tweets#new'
+  post  'tweets'      =>  'tweets#create'    
+
+
+  #ツイート投稿機能
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
