c459c22882bc4774d40fa6e60efdd4c2e31f3863
土屋太 <tsuchiyafutoshi@tsuchiyafutoshi-no-MacBook-Air.local>
2016-07-08 16:48:47 +0900

    コメント機能完成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -33,8 +33,12 @@ class TweetsController < ApplicationController
    end
     private
     def tweet_params
-      params.permit(:title, :video, :text)
+      # video = video
+      # video.slice!(1..32)
+      params.permit(:title, :video, :text)  
+
     end
+
     def move_to_index
       redirect_to action: :index unless user_signed_in?
     end
